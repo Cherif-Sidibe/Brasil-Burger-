0af29092b45d5fd31b85b5e2e3fca65576eaadde
Cherif Sidibe <your-email@example.com>
2025-12-12 00:47:58 +0000

    Java: Création Entity

---
+++ java/app/src/main/java/sidibe/cherif/entity/Zone.java
@@ -0,0 +1,24 @@
+package sidibe.cherif.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;       
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter 
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Zone {
+    private int id;
+    private String nom;
+    private List<String> quartiers;
+    private double prixLivraison;
+    private boolean isArchive;
+    private LocalDate createAt;
+    private LocalDate updateAt;
+}

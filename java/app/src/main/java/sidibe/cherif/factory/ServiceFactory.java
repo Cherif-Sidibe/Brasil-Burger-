73ed2767bcbb985352e89e44a7b4f550d5a17ac5
Cherif Sidibe <your-email@example.com>
2025-12-12 14:26:07 +0000

    Creation de la factory

---
+++ java/app/src/main/java/sidibe/cherif/factory/ServiceFactory.java
@@ -0,0 +1,98 @@
+package sidibe.cherif.factory;
+
+import sidibe.cherif.repository.*;
+import sidibe.cherif.repository.impl.*;
+import sidibe.cherif.service.*;
+import sidibe.cherif.service.impl.*;
+
+
+public class ServiceFactory {
+    
+    private static ServiceFactory instance;
+    
+    private BurgerRepository burgerRepository;
+    private MenuRepository menuRepository;
+    private ComplementRepository complementRepository;
+    private ZoneRepository zoneRepository;
+    private UserRepository userRepository;
+    
+    private BurgerService burgerService;
+    private MenuService menuService;
+    private ComplementService complementService;
+    private ZoneService zoneService;
+    private UserService userService;
+    
+    
+    private ServiceFactory() {
+        initializeRepositories();
+        initializeServices();
+    }
+    
+    
+    public static ServiceFactory getInstance() {
+        if (instance == null) {
+            instance = new ServiceFactory();
+        }
+        return instance;
+    }
+    
+    
+    private void initializeRepositories() {
+        burgerRepository = new BurgerRepositoryImpl();
+        menuRepository = new MenuRepositoryImpl();
+        complementRepository = new ComplementRepositoryImpl();
+        zoneRepository = new ZoneRepositoryImpl();
+        userRepository = new UserRepositoryImpl();
+    }
+    
+    
+    private void initializeServices() {
+        burgerService = new BurgerServiceImpl(burgerRepository);
+        menuService = new MenuServiceImpl(menuRepository);
+        complementService = new ComplementServiceImpl(complementRepository);
+        zoneService = new ZoneServiceImpl(zoneRepository);
+        userService = new UserServiceImpl(userRepository);
+    }
+    
+    
+    public BurgerService getBurgerService() {
+        return burgerService;
+    }
+    
+    public MenuService getMenuService() {
+        return menuService;
+    }
+    
+    public ComplementService getComplementService() {
+        return complementService;
+    }
+    
+    public ZoneService getZoneService() {
+        return zoneService;
+    }
+    
+    public UserService getUserService() {
+        return userService;
+    }
+    
+    
+    public BurgerRepository getBurgerRepository() {
+        return burgerRepository;
+    }
+    
+    public MenuRepository getMenuRepository() {
+        return menuRepository;
+    }
+    
+    public ComplementRepository getComplementRepository() {
+        return complementRepository;
+    }
+    
+    public ZoneRepository getZoneRepository() {
+        return zoneRepository;
+    }
+    
+    public UserRepository getUserRepository() {
+        return userRepository;
+    }
+}
